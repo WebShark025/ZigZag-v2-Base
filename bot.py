f0397db14b56faa5cd594aaa06555c1401112bc4
Arshia shakeri <arshshia@gmail.com>
2017-06-22 19:25:50 +0430

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+####################################
+# The ZigZag Project v2 !          #
+# Author: WebShark25               #
+# (Mohammad Arshia Seyyed Shakeri) #
+####################################
+#                                  #
+# Copyright @2017 iTeam Product    #
+#                                  #
+####################################
+
+
+
+# Check for CONFIG file and LOCALE file
+if not os.path.exists("config.py"):
+  copyfile("data/config.default.py", "config.py")
+if not os.path.exists("locale.py"):
+  copyfile("data/locale.default.py", "locale.py")
+
+# Connect to Redis database
+redisserver = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+# Set *default encoding*
+reload(sys)  
+sys.setdefaultencoding("utf-8")
+
+# Load config and locale file into the system. 
+execfile("locale.py")
+execfile("config.py")
+
+# Start the bot
+bot = telebot.TeleBot(TOKEN)
+
+# Load plugins
+for plugin in enabled_plugins:
+  try:
+    execfile("plugins/" + plugin + ".py")
+    print("Plugin enabled successfully: " + plugin)
+  except Exception as ex:
+    print("Could enable plugin: " + plugin + ". Error:")
+    print(ex)
+
+print("Bot launched successfully. Launch time: " + str(time))
+
+# Set message handler!
+bot.set_update_listener(message_replier)
+
+# Poll! Lets go.
+bot.polling(none_stop=True, interval=0, timeout=3)
